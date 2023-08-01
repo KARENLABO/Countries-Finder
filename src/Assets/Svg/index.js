b67dee86a385d8445b3ae02b2e1e36ded99ecc9e
karen <karen.bussines@hotmail.com>
2023-08-01 13:46:14 -0600

    Feat: Create and homologate names of icons

--- src/Assets/Svg/index.js
+++ src/Assets/Svg/index.js
@@ -1,2 +1,5 @@
-export { default as Logo } from "./Logo";
-export { default as Search } from "./Search";
+export { default as Logo } from './Logo';
+export { default as Search } from './Search';
+export { default as Wrong } from './Wrong';
+export { default as Back } from './Back';
+export { default as Restart } from './Restart';
