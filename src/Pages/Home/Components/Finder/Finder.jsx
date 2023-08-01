0e00b3ee2e4d8b4257dfbdfe3a5de7d682330502
karen <karen.bussines@hotmail.com>
2023-07-31 20:11:34 -0600

    Refactor: Update Finder component, change it by autocomplete from ant design

--- src/Pages/Home/Components/Finder/Finder.jsx
+++ src/Pages/Home/Components/Finder/Finder.jsx
@@ -1,20 +1,63 @@
-import React from 'react';
-import { Search } from '../../../../Assets/Svg';
+import React, { useState } from 'react';
+import { AutoComplete, Input } from 'antd';
+import { func } from 'prop-types';
+
+import { fetchDataByName } from '../../../../Api';
 import './styles.scss';
 
-function Finder() {
+function Finder({ setInfo }) {
+  const [options, setOptions] = useState([]);
+
+  const searchResult = async (value, clicked) => {
+    const { results } = await fetchDataByName(value);
+    if (clicked) {
+      setInfo(results);
+    }
+
+    return results?.map((res) => ({
+      value: `${res.name}`,
+      label: (
+        <div>
+          <span>
+            Found <b>{value.toUpperCase()}</b> on {res.name}
+          </span>
+        </div>
+      ),
+    }));
+  };
+
+  const handleSearch = async (value) => {
+    const res = await searchResult(value);
+    setOptions(value ? res : []);
+  };
+
+  const onSelect = async (value) => {
+    const { results } = await fetchDataByName(value);
+    setInfo(results);
+  };
+
   return (
     <div className="finder-container">
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Type to search"
-      />
-      <button type="submit" className="search-icon-container">
-        <img src={Search} alt="search-icon" className="search-icon" />
-      </button>
+      <AutoComplete
+        popupMatchSelectWidth={252}
+        style={{
+          width: 500,
+        }}
+        options={options}
+        onSelect={onSelect}
+        onSearch={handleSearch}
+      >
+        <Input.Search
+          size="large"
+          placeholder="Type something to search"
+          enterButton
+        />
+      </AutoComplete>
     </div>
   );
 }
+Finder.propTypes = {
+  setInfo: func.isRequired,
+};
 
 export default Finder;
